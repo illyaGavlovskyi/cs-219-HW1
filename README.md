2804118c97e7301bb7d1527e110647629a0a6092
illyaGavlovskyi <151693791+illyaGavlovskyi@users.noreply.github.com>
2024-03-24 03:10:36 +0000

    1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-CS 219 Programming Project 1
+CS 219 Programming Project 2
 
 to run this code you will run make and ./adder in the terminal.
 
@@ -6,6 +6,6 @@ the data that is stored inside data.txt will be read in. It has to be in the for
 
 This program from the start declares 3 strings and 3 uint32_t data type variables. strings are used by gettline to extract data from a file. I start by opening an ifstream and checking if it opens, if not program returns 0. After that, a while loop opens that runs until the file is empty. 
 
-Every iteration starts by emptying all 3 strings to ensure that no extra data in the output. The data is collected using getlines. to convert string to uint32_t i used stoul( ). If ADD operation is selected the code keeps running otherwise program ends. a variable of type stringstream from sstream library gets created and a function <<hex<< is used to convert a hexadecimal number to a string type. after that, I create a string that gets set to a value from the stringstream with the use of .str(). this allows to use cout and displays the correct value to the user. 
+Every iteration starts by emptying all 3 strings to ensure that no extra data in the output. The data is collected using getlines. to convert string to uint32_t i used stoul( ). If (ADD, AND, ORR, SUB, XOR, ASR, LSR,LSL, NOT, ADDS, ANDS, ORRS, SUBS XORS, ASRS, LSRS, LSLS, NOTS) operation is selected the code keeps running otherwise program ends. a variable of type stringstream from sstream library gets created and a function <<hex<< is used to convert a hexadecimal number to a string type. after that, I create a string that gets set to a value from the stringstream with the use of .str(). this allows to use cout and displays the correct value to the user. 
 
 But before that, there is another function that is made to set all the values inside the string to the upper case to have the correct format. 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 //Illya Gavlovskyi
-//Programming project 1
+//Programming project 2
 
 #include <stdint.h>
 #include <iostream>
@@ -9,10 +9,11 @@
 using namespace std;
 
 string stringUp(string operation);
+void flags(int32_t num);
 
 int main(){
     string operation, firstS, secondS;
-    uint32_t first, second, third;
+    
 
     ifstream inputStream("data.txt");
     if(!inputStream.is_open()){
@@ -23,39 +24,278 @@ int main(){
         operation = "";
         firstS = "";
         secondS = "";
-        // inputStream>>operation>>firstS>>secondS;
+
         getline(inputStream, operation, ' ');
-        getline(inputStream, firstS, ' ');
-        getline(inputStream, secondS);
+        if(operation == "ADD" || operation == "AND" || operation == "ORR" || operation == "SUB" || operation == "XOR"){
+            uint32_t first, second, third;
+            getline(inputStream, firstS, ' ');
+            getline(inputStream, secondS);
+            // cout<<"Enter operation and hexadecimal numbers:";
+            // cin>>operation;
+            // cin>>firstS;
+            // cin>>secondS;
+            cout<<operation<<"  "<<firstS<<"  "<<secondS<<": ";
+            first = stoul(firstS, nullptr, 16);
+            second = stoul(secondS, nullptr, 16);
+            
+            if(operation == "ADD"){
+                third = first + second;
+            }
+            else if(operation == "AND"){
+                third = first & second;
+            }
+            else if(operation == "ORR"){
+                third = first | second;
+            }
+            else if(operation == "SUB"){
+                third = first - second;
+            }
+            else if(operation == "XOR"){
+                third = first ^ second;
+            }
+            else{
+                cout<<"Not working"<<endl;
+                return 0;
+            }
 
-        cout<<operation<<"  "<<firstS<<"  "<<secondS<<": ";
-        // cout<<"Enter operation and hexadecimal numbers:";
-        // cin>>operation;
-        // cin>>firstS;
-        // cin>>secondS;
+            stringstream stream;
+            stream<<hex<< third;
+            string hexString = stream.str();
 
-        first = stoul(firstS, nullptr, 16);
-        second = stoul(secondS, nullptr, 16);
+            hexString = stringUp(hexString);
+            cout<<"0x";
+            for(int i = 0; i < 8 - hexString.length();i++){
+                cout<<"0";
+            }
+            cout<<hexString<<endl;
+        }
         
-        if(operation == "ADD"){
-            third = first + second;
-        }
-        else{
-            cout<<"Not working"<<endl;
-            return 0;
+        else if(operation == "ASR"){
+            int second;
+            int32_t first, third;
+            getline(inputStream, firstS, ' ');
+            getline(inputStream, secondS);
+            
+            cout<<operation<<"  "<<firstS<<"  "<<secondS<<": ";
+            first = stoul(firstS, nullptr, 16);
+            second = stoi(secondS);
+
+            if(operation == "ASR"){
+                third = first >> second;
+            }
+            else{
+                cout<<"Not working"<<endl;
+                return 0;
+            }
+            stringstream stream;
+            stream<<hex<< third;
+            string hexString = stream.str();
+
+            hexString = stringUp(hexString);
+            cout<<"0x";
+            for(int i = 0; i < 8 - hexString.length();i++){
+                cout<<"0";
+            }
+            cout<<hexString<<endl;
         }
 
-        stringstream stream;
-        stream<<hex<< third;
-        string hexString = stream.str();
+        else if(operation == "LSR" || operation == "LSL"){
+            int second;
+            uint32_t first, third;
+            getline(inputStream, firstS, ' ');
+            getline(inputStream, secondS);
+            
+            cout<<operation<<"  "<<firstS<<"  "<<secondS<<": ";
+            first = stoul(firstS, nullptr, 16);
+            second = stoi(secondS);
 
-        hexString = stringUp(hexString);
-        cout<<"0x";
-        for(int i = 0; i < 8 - hexString.length();i++){
-            cout<<"0";
+            if(operation == "LSR"){
+                third = first >> second;
+            }
+            else if(operation == "LSL"){
+                third = first << second;
+            }
+            else{
+                cout<<"Not working"<<endl;
+                return 0;
+            }
+            stringstream stream;
+            stream<<hex<< third;
+            string hexString = stream.str();
+
+            hexString = stringUp(hexString);
+            cout<<"0x";
+            for(int i = 0; i < 8 - hexString.length();i++){
+                cout<<"0";
+            }
+            cout<<hexString<<endl;
+
         }
-        cout<<hexString<<endl;
+
+        else if(operation == "NOT"){
+            uint32_t first, third;
+            getline(inputStream, firstS);
+            cout<<operation<<"  "<<firstS<<": ";
+            first = stoul(firstS, nullptr, 16);    
+
+            if(operation == "NOT"){
+                third = ~first;
+            }
+            else{
+                cout<<"Not working"<<endl;
+                return 0;
+            }
+            stringstream stream;
+            stream<<hex<< third;
+            string hexString = stream.str();
+
+            hexString = stringUp(hexString);
+            cout<<"0x";
+            for(int i = 0; i < 8 - hexString.length();i++){
+                cout<<"0";
+            }
+            cout<<hexString<<endl;
+        }
+        
+        /////////////////////////////////////////
+
+        if(operation == "ADDS" || operation == "ANDS" || operation == "ORRS" || operation == "SUBS" || operation == "XORS"){
+            int32_t first, second, third;
+            getline(inputStream, firstS, ' ');
+            getline(inputStream, secondS);
+            // cout<<"Enter operation and hexadecimal numbers:";
+            // cin>>operation;
+            // cin>>firstS;
+            // cin>>secondS;
+            cout<<operation<<"  "<<firstS<<"  "<<secondS<<": ";
+            first = stoul(firstS, nullptr, 16);
+            second = stoul(secondS, nullptr, 16);
+            
+            if(operation == "ADDS"){
+                third = first + second;
+            }
+            else if(operation == "ANDS"){
+                third = first & second;
+            }
+            else if(operation == "ORRS"){
+                third = first | second;
+            }
+            else if(operation == "SUBS"){
+                third = first - second;
+            }
+            else if(operation == "XORS"){
+                third = first ^ second;
+            }
+            else{
+                cout<<"Not working"<<endl;
+                return 0;
+            }
+
+            stringstream stream;
+            stream<<hex<< third;
+            string hexString = stream.str();
+
+            hexString = stringUp(hexString);
+            cout<<"0x";
+            for(int i = 0; i < 8 - hexString.length();i++){
+                cout<<"0";
+            }
+            cout<<hexString<<endl;
+            flags(third);
+            
+        }
+        
+        else if(operation == "ASRS"){
+            int second;
+            int32_t first, third;
+            getline(inputStream, firstS, ' ');
+            getline(inputStream, secondS);
+            
+            cout<<operation<<"  "<<firstS<<"  "<<secondS<<": ";
+            first = stoul(firstS, nullptr, 16);
+            second = stoi(secondS);
+
+            if(operation == "ASRS"){
+                third = first >> second;
+            }
+            else{
+                cout<<"Not working"<<endl;
+                return 0;
+            }
+            stringstream stream;
+            stream<<hex<< third;
+            string hexString = stream.str();
+
+            hexString = stringUp(hexString);
+            cout<<"0x";
+            for(int i = 0; i < 8 - hexString.length();i++){
+                cout<<"0";
+            }
+            cout<<hexString<<endl;
+            flags(third);
+        }
+
+        else if(operation == "LSRS" || operation == "LSLS"){
+            int second;
+            int32_t first, third;
+            getline(inputStream, firstS, ' ');
+            getline(inputStream, secondS);
+            
+            cout<<operation<<"  "<<firstS<<"  "<<secondS<<": ";
+            first = stoul(firstS, nullptr, 16);
+            second = stoi(secondS);
+
+            if(operation == "LSRS"){
+                third = first >> second;
+            }
+            else if(operation == "LSLS"){
+                third = first << second;
+            }
+            else{
+                cout<<"Not working"<<endl;
+                return 0;
+            }
+            stringstream stream;
+            stream<<hex<< third;
+            string hexString = stream.str();
+
+            hexString = stringUp(hexString);
+            cout<<"0x";
+            for(int i = 0; i < 8 - hexString.length();i++){
+                cout<<"0";
+            }
+            cout<<hexString<<endl;
+            flags(third);
+        }
+
+        else if(operation == "NOTS"){
+            int32_t first, third;
+            getline(inputStream, firstS);
+            cout<<operation<<"  "<<firstS<<": ";
+            first = stoul(firstS, nullptr, 16);    
+
+            if(operation == "NOTS"){
+                third = ~first;
+            }
+            else{
+                cout<<"Not working"<<endl;
+                return 0;
+            }
+            stringstream stream;
+            stream<<hex<< third;
+            string hexString = stream.str();
+
+            hexString = stringUp(hexString);
+            cout<<"0x";
+            for(int i = 0; i < 8 - hexString.length();i++){
+                cout<<"0";
+            }
+            cout<<hexString<<endl;
+            flags(third);
+        }
+        
     }
+    
     inputStream.close();
     return 0;
 }
@@ -69,4 +309,19 @@ string stringUp(string operation){
         }
     }
     return operation;
+}
+
+void flags(int32_t num){
+    if(num < 0){
+        cout<<"N:1 ";
+    }
+    else{
+        cout<<"N:0 ";
+    }
+    if(num == 0){
+        cout<<"Z:1 "<<endl;
+    }
+    else{
+        cout<<"Z:0 "<<endl;
+    }
 }
